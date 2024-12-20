4641d5a96d18615839614a0ef46429df3f950f9c
Azhar <azharadityapratama48@gmail.com>
2024-12-20 14:12:32 +0700

    add metadata

--- src/main/java/org/example/AIPlayer.java
+++ src/main/java/org/example/AIPlayer.java
@@ -1,3 +1,13 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #6
+ * 1 - 5026231084 - Azhar Aditya Pratama
+ * 2 - 5026231109 - Abdul Ghoni
+ * 3 - 5026231200 - Cristo Pison Ben Jarred
+ */
+
 package org.example;
 
 public abstract class AIPlayer {
